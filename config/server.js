234c001ddcdbbb6e1ad5bf12b11b0b888a6153e0
jwchan <741755613@qq.com>
2021-02-23 17:29:39 +0800

    feat: init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3faddf64ab27a4bf62d72bb288a04d5f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3b47f6b9-5c4b-49f9-b1be-7a8f6a8a9fb8'
+};
